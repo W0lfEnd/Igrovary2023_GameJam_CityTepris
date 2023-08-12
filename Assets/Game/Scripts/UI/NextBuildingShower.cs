ca69b5f1599c7a9950a7791c39dd06f669496025
OleksiyRudakov <rudakovoleksiy777@gmail.com>
2023-08-12 22:05:13 +0300

    Added basic next building shower in UI canvas.

---
+++ Assets/Game/Scripts/UI/NextBuildingShower.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Scripts.UI
+{
+    public class NextBuildingShower : MonoBehaviour
+    {
+        [SerializeField] private BuildingVectors[] _buildingVectors;
+        [SerializeField] private Sprite _buildingSprite;
+        private Color _emptyColor = new Color(0f, 0f, 0f, 0f);
+        private Color _normalColor = new Color(255f, 255f, 255f, 1f);
+
+        private void Show(Vector2Int[] vectors)
+        {
+            Clear();
+            ConfigurateTiles(vectors);
+        }
+
+        private void ConfigurateTiles(Vector2Int[] vectors)
+        {
+            for (int i = 0; i < _buildingVectors.Length; i++)
+            {
+                for (int j = 0; j < vectors.Length; j++)
+                {
+                    if (_buildingVectors[i].buildingVector == vectors[j])
+                        _buildingVectors[i].imageComponent.color = _normalColor;
+                }
+            }
+        }
+
+        private void Clear()
+        {
+            for (int i = 0; i < _buildingVectors.Length; i++)
+            {
+                _buildingVectors[i].imageComponent.color = _emptyColor;
+            }
+        }
+    }
+
+    [Serializable]
+    public struct BuildingVectors
+    {
+        public Vector2Int buildingVector;
+        public Image imageComponent;
+    }
+}
