0d0cdd1d4d73f90bf52532bc7365aa3308d7131e
BogdanMasliy <masliy45@gmail.com>
2023-08-12 20:15:38 +0300

    develop world swap

---
+++ Assets/Game/Scripts/World/WorldAnimSwapper.cs
@@ -0,0 +1,50 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Game.Scripts.World
+{
+    public class WorldAnimSwapper : MonoBehaviour
+    {
+        [SerializeField] private Camera _targetCamera;
+        [SerializeField] private Transform _to;
+        [SerializeField] private float _duration;
+
+        public Action OnCompleteSwap;
+        public Action OnStartSwap;
+
+        public void SwapToDown()
+        {
+            OnStartSwap?.Invoke();
+            DOTween.Kill(_targetCamera.transform);
+
+            MoveYAnimation(_targetCamera.gameObject, _duration, 0, _to.position.y, () => { OnCompleteSwap?.Invoke(); });
+            RotateAnimation(_targetCamera.gameObject, _duration, new Vector3(0, 0, -180), (() => { }));
+        }
+
+        public void SwapToUp()
+        {
+            OnStartSwap?.Invoke();
+            DOTween.Kill(_targetCamera.transform);
+
+            MoveYAnimation(_targetCamera.gameObject, _duration, 0, 0, (() => { OnCompleteSwap?.Invoke(); }));
+            //_targetCamera.transform.localRotation= Quaternion.Euler(0f, 0f, 180f);
+            RotateAnimation(_targetCamera.gameObject, _duration, new Vector3(0, 0, 0), (() => { }));
+        }
+
+        public void MoveYAnimation(GameObject gameObject, float duration, float delay, float yPosition,
+            Action onComplete)
+        {
+            gameObject.transform.DOMoveY(yPosition, duration).SetEase(Ease.OutQuad)
+                .SetDelay(delay)
+                .OnComplete(() => { onComplete?.Invoke(); });
+        }
+
+        public void RotateAnimation(GameObject gameObject, float duration, Vector3 rotate, Action onComplete)
+        {
+            gameObject.transform.DORotate(rotate, duration)
+                .SetEase(Ease.OutQuad)
+                .OnComplete(() => { onComplete?.Invoke(); });
+        }
+    }
+}
