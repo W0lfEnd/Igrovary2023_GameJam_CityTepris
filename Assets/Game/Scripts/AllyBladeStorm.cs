1b75c0c89d6bb80a1258a6e772f797e1cb13078a
andrii.oliinyk <oliinyk.dev@gmail.com>
2023-08-12 22:37:49 +0300

    added "blade storm" upgrade for main building

---
+++ Assets/Game/Scripts/AllyBladeStorm.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Enemies;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.Serialization;
+
+
+public class AllyBladeStorm : MonoBehaviour
+{
+    [SerializeField] private Transform            bladesRoot         = null;
+    [SerializeField] private AllyBladeStorm_Blade go_blade           = null;
+    [SerializeField] private int                  Damage             = 1;
+    [SerializeField] private float                RotationSpeedInDeg = 10f;
+    [SerializeField] private float                AttackRadius       = 10f;
+
+    private List<AllyBladeStorm_Blade> bladesList = new List<AllyBladeStorm_Blade>();
+
+
+    public void init( int bladesCount = 5 )
+    {
+        if ( bladesList.Count < bladesCount )
+        {
+            int bladesToSpawn = bladesCount - bladesList.Count;
+            for ( int i = 0; i < bladesToSpawn; i++ )
+            {
+                AllyBladeStorm_Blade newBlade = Instantiate( go_blade, Vector3.zero, Quaternion.identity, bladesRoot ).GetComponent<AllyBladeStorm_Blade>();
+                bladesList.Add( newBlade );
+            }
+        }
+
+        initBlades();
+    }
+
+    private void initBlades()
+    {
+        float radianBladeOffset = 2 * Mathf.PI / bladesList.Count;
+        for ( int i = 0; i < bladesList.Count; i++ )
+        {
+            bladesList[i].init( Damage );
+            bladesList[i].transform.localPosition = AttackRadius * new Vector3( Mathf.Sin( i * radianBladeOffset ), Mathf.Cos( i * radianBladeOffset ), 0 );
+            bladesList[i].transform.right = (transform.position - bladesList[i].transform.position).normalized;
+
+        }
+    }
+
+    private void Update()
+    {
+        bladesRoot.Rotate( 0,0, Time.deltaTime * RotationSpeedInDeg );
+    }
+    
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere( transform.position, AttackRadius );
+    }
+}
---
+++ Assets/Game/Scripts/AllyBladeStorm_Blade.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Enemies;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.Serialization;
+
+
+public class AllyBladeStorm_Blade : MonoBehaviour
+{
+  [SerializeField] private int damage = 10;
+
+  public void init( int damage )
+  {
+    this.damage = damage;
+  }
+
+  private void OnCollisionEnter2D( Collision2D col )
+  {
+    BaseEnemy enemy = col.gameObject.GetComponent<BaseEnemy>();
+    if ( enemy )
+    {
+      enemy.Damage( damage );
+    }
+  }
+}
