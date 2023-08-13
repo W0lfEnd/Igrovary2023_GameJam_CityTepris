a6c164a8854f93c232a4f8d1bc134f273e5adb68
andrii.oliinyk <oliinyk.dev@gmail.com>
2023-08-13 13:07:48 +0300

    up

--- Assets/Game/Scripts/UI/UiBar.cs
+++ Assets/Game/Scripts/UI/UiBar.cs
@@ -1,36 +1,34 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Game.Scripts.UI
 {
     public class UiBar : MonoBehaviour
     {
-        [SerializeField] private Slider _slider;
-        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private Slider          _slider;
+        [SerializeField] private TextMeshProUGUI _title;
+        [SerializeField] private TextMeshProUGUI _values;
 
 
-        private Func<int, string> get_text_func = null;
+        private Func<int, string> get_values_func = null;
+        private Func<int, string> get_title_func = null;
 
         public Slider Slider => _slider;
 
         private void Awake()
         {
-            _slider.onValueChanged.AddListener( val =>
-            {
-                if ( !_text || get_text_func == null )
-                    return;
-
-                _text.text = get_text_func( (int)val );
-                return;
-            } );
+            _slider.onValueChanged.AddListener( updateTitleText );
+            _slider.onValueChanged.AddListener( updateValuesText );
         }
 
-        public void Initialize(int maxValue, Func<int, string> get_text_func = null)
+        public void Initialize(int maxValue, Func<int, string> get_title_func = null, Func<int, string> get_values_func = null )
         {
             _slider.maxValue = maxValue;
-            this.get_text_func = get_text_func;
+            this.get_title_func = get_title_func;
+            this.get_values_func = get_values_func;
         }
 
         public void IncreaseValue(int value)
@@ -41,6 +39,22 @@ namespace Game.Scripts.UI
         public void SetValue(int value)
         {
             _slider.value = value;
+        }
+
+        private void updateTitleText(float val)
+        {
+            if ( !_title || get_title_func == null )
+                return;
+
+            _title.text = get_title_func( (int)val );
+        }
+
+        private void updateValuesText(float val)
+        {
+            if ( !_title )
+                return;
+
+            _values.text = get_values_func != null ? get_values_func( (int)val ) : $"{(int)val}/{Slider.maxValue}";
         }
     }
 }
